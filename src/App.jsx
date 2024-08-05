e022346e617ed5f1d37b128a47639f14194d3b57
Khushwant <khushwantmanglani6@gmail.com>
2024-08-05 18:26:51 +0530

    Setup authentication with redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./features/authSlice";
+import authService from "./appwrite/authService";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
+// import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1>A react full stack blog app with appwrite</h1>
-    </>
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    authService
+      .getAccount()
+      .then((userData) => {
+        if (userData) {
+          useDispatch(login({ userData }));
+        } else {
+          useDispatch(logout());
+        }
+      })
+      // .catch((err) => setError(err.message))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (error.length) {
+    return <div>Error Occurred: {error}</div>;
+  }
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>TODO: {/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div>Loading</div>
   );
 }
 
